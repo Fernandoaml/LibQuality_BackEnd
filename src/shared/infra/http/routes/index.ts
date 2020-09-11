b364b29e16a7464d4126200676bf4e126f1868f6
Fernando A M Leite <fernandomottaptc@gmail.com>
2020-09-11 00:54:10 -0300

    Implemented Swagger Ui and created api doc

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import * as swagerDocument from '@shared/infra/services/swagger.json';
 
 import repositoriesRouter from '@modules/issues/infra/http/routes/repositories.routes';
 
 const routes = Router();
 
 routes.use('/repositories', repositoriesRouter);
+routes.use('/swagger', swaggerUi.serve, swaggerUi.setup(swagerDocument));
 
 export default routes;
